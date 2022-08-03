75a02abb6d3848d550e7b09cfd14fb488cda7fb2
Horacio <horacio.junior@ibm.com>
2022-08-03 07:08:48 -0300

    Initial Commit

---
+++ Lista-De-Tarefas/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lista-De-Tarefas
+//
+//  Created by Horacio Gomes dos Santos Junior on 03/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
